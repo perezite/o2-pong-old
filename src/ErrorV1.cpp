c9f05cd331f902690dc6f0015c2dbf6080b01316
simulo <simulo2000@gmail.com>
2020-07-30 23:42:34 +0200

    Minor

--- src/ErrorV1.cpp
+++ src/ErrorV1.cpp
@@ -12,16 +12,16 @@ namespace
 		void debug(const string& message) {
 			int result;
 
-			const SDL_MessageBoxButtonData buttons[] = 
+			const SDL_MessageBoxButtonData buttons[] =
 			{
 				{ SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT, 0, "Quit" },
 				{ SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT, 1, "Debug" }
 			};
 
-			const SDL_MessageBoxData messageboxdata = 
-			{ 
-				SDL_MESSAGEBOX_ERROR, NULL, "A problem occured", 
-				message.c_str(), SDL_arraysize(buttons), buttons, NULL 
+			const SDL_MessageBoxData messageboxdata =
+			{
+				SDL_MESSAGEBOX_ERROR, NULL, "A problem occured",
+				message.c_str(), SDL_arraysize(buttons), buttons, NULL
 			};
 
 			SDL_ShowMessageBox(&messageboxdata, &result);
@@ -39,9 +39,9 @@ namespace o2
 		void error(const string& message) {
 			#if defined(_MSC_VER) && defined(_DEBUG)
 				debug(message);
-			#else	
+			#else
 				SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "A problem occured", message.c_str(), NULL);
-			#endif	
+			#endif
 
 			exit(0);
 		}
